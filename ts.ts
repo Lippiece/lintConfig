af442b94de1695ee184dbc87ac43b4ec6e151595
lippiece <github@lippiece.anonaddy.me>
2026-01-09 14:57:22 +0200

    remove dummy file

--- ts.ts
+++
@@ -1,7 +0,0 @@
-export const object = {}
-const a   = 4
-const bas = "saefjaelfajefalkefjalekjaekjaelkjaelkfjaelkfjalekjaelkjalekfjakejfk"
-
-export const key = () =>
-  "name ajkef  kaejfk :w  jaajkefj kajjk aaefjkjkjw aejfkj jakkeka"
-object[key()] = "foo"
